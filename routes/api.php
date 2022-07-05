dfe8e7a935aa6911d9512efc68196616baa80718
Joseph Bassey <Joemires20@yahoo.com>
2022-07-05 17:54:43 +0100

    added user controller class with resource route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('api-responder')->group( function() {
-
+    Route::resource('user', UserController::class)->only('index', 'show');
 });
